2ecbca54b56f83f95583b587e53e5ac88682d8de
Tim C <timoman8@hotmail.com>
2018-04-01 18:10:43 -0400

    Finish from laptop

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
+let AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+Object.defineProperty(Array.prototype, 'remove', {
+  value: function(fArray){
+    return this.filter((a)=>{
+      return !fArray.includes(a);
+    });
+  },
+  writable: false,
+  enumerable: false
+});
+Object.defineProperty(Array.prototype, 'kvify', {
+  value: function(){
+    let rv=[];
+    this.forEach((e)=>{
+      rv.push(e+':'+e);
+    });
+    return rv;
+  },
+  writable: false,
+  enumerable: false
+});
 function RESTAPI(config){
   let self = this;
-  Object.defineProperty(this, 'internalCall', {
-    value: function(url, method, data){
-      function urlify(url, data){
-        let params = '';
-        let keyNames = Object.keys(data);
-        keyNames.forEach((kn)=>{
-          let kv = data[kn];
-          params += (keyNames.indexOf(kn)==0?`?${kn}=${kv}`:`&${kn}=${kv}`);
-        });
-        return url+params;
-      }
-      let newUrl = urlify(url, data);
-      console.log(newUrl)
-    },
-    writable: false,
-    enumeralbe: false
-  });
   this.defaultConfig = {
     restAPI: 'https://jsonplaceholder.typicode.com/',
     parameters: ['method','resource','data'],
@@ -25,6 +29,15 @@ function RESTAPI(config){
       ['resource', 'posts', 'comments', 'albums', 'photos', 'todos', 'users'],
       ['data', 'Object']
     ],
+    paramTransform: {
+      'path': 'resource'
+    },
+    excludeFromData: [
+      'data',
+      'headers',
+      'resource',
+      'method'
+    ],
     description: `defaultAPI(method, resource, data)
     Send a request to $$restAPI$$(config)
     method is one of the following: $$method$$(multi)
@@ -32,15 +45,22 @@ function RESTAPI(config){
     data is of type $$data$$(single)`,
     example: function(){
       console.log('%c%s', 'color: blue;', 'Called example("GET", "posts", {userId: 1})');
-      self.internalCall(`https://jsonplaceholder.typicode.com/posts`, 'GET', {
-        userId: 1
-      });
+      return self.internalCall('https://jsonplaceholder.typicode.com/posts',"GET",{userId: 1},{});
     }
   };
   this.config = config || this.defaultConfig;
-  let CONSTRUCTEDAPI = new Function(...this.config.parameters, `
-    
+  let CONSTRUCTEDAPI = new AsyncFunction(...this.config.parameters, `
+    let self = RESTAPI;
+    return await self.prototype.internalCall('${this.config.restAPI}${this.config.paramTransform?'\'+'+this.config.paramTransform.path+'+\'':''}',
+    ${this.config.parameters.includes('method')?'method':'\"GET\"'},
+    ${this.config.parameters.includes('data')?'data':'{\n'+this.config.parameters.remove(this.config.excludeFromData||['data', 'headers']).kvify().join(',\n')+'\n}'},
+    ${this.config.parameters.includes('headers')?'headers':'{}'});
     `);
+  Object.defineProperty(CONSTRUCTEDAPI, 'internalCall', {
+    value: self.internalCall,
+    writable: false,
+    enumerable: true
+  });
   if(this.config.description){
     CONSTRUCTEDAPI.toString = function(){
       let descriptorRegEx = new RegExp('\\$\\$([\\w]*)\\$\\$\\(([\\w]*)\\)', 'g')
@@ -70,12 +90,43 @@ function RESTAPI(config){
       return parsedDescription;
     };
   }
-  CONSTRUCTEDAPI.example = this.config.example || function(){console.log('%c%s', "color: red;", 'No example present.')};
+  CONSTRUCTEDAPI.example = this.config.example || function(){
+    console.log('%c%s', "color: red;", 'No example present.');
+  };
   return CONSTRUCTEDAPI;
 }
-
-(function Test(){
+(function(){
+  async function __internalCall__(url, method, data, headers){
+    //console.log(url, method, data, headers)
+    return await $.ajax({
+      url: url,
+      method: method,
+      // dataType: 'json'
+      data: data,
+      headers: headers,
+      success: function(data){
+        return {
+          data: data,
+          result: 'success'
+        };
+      },
+      error: function(data){
+        return {
+          data: data,
+          result: 'error'
+        };
+      }
+    });
+  }
+  Object.defineProperty(RESTAPI.prototype, 'internalCall', {
+    value: __internalCall__,
+    writable: false,
+    enumerable: true
+  });
+})();
+(async function Test(){
   console.log("%c%s","background-color: black; color: white; text-align: center;", "Running tests");
+  console.log("%c%s","background-color: black; color: white; text-align: center;", "YQLQuery test");
   let YQLQuery = new RESTAPI({
     restAPI: 'http://query.yahooapis.com/v1/public/yql',
     parameters: [
@@ -83,11 +134,34 @@ function RESTAPI(config){
       'format',
       'diagnostics',
       'env'
-    ]
+    ],
+    parameterTypes: [
+      ['q', 'String'],
+      ['format', 'json'],
+      ['diagnostics', 'true', 'false'],
+      ['env', 'store://datatables.org/alltableswithkeys']
+    ],
+    excludeFromData: [
+      'data',
+      'headers',
+      'method'
+    ],
+    description: `YQLQuery(q, format, diagnostics, env)
+    Send a request to $$restAPI$$(config)
+    q is a $$q$$(single) following the yql query syntax
+    format is one of the following data types: $$format$$(single)
+    diagnostics is one of the following: $$diagnostics$$(multi)
+    env is a url of a yqltable database. The default ones are $$env$$(multi)
+    `
   });
   console.log(YQLQuery);
+  console.log("%c%s","background-color: black; color: white; text-align: center;", "YQLQuery select * from htmlstring where url='www.google.com'");
+  console.log(await YQLQuery("select * from htmlstring where url='www.google.com'", 'json', true, 'store://datatables.org/alltableswithkeys'))
+  console.log("%c%s","background-color: black; color: white; text-align: center;", "Placeholder/defaultAPI test");
   let placeholderAPI = new RESTAPI();
   console.log(placeholderAPI);
+  console.log("%c%s","background-color: black; color: white; text-align: center;", "Placeholder example call");
   console.log(placeholderAPI.example());
-  console.log(placeholderAPI.internalCall)
+  console.log("%c%s","background-color: black; color: white; text-align: center;", "Placeholder get post with userId: 1");
+  console.log(await placeholderAPI("GET", 'posts', {userId: 1}));
 })();
